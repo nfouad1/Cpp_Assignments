97c8b64588c3d2d3a6795740cefeb284de32d5c8
Noureldin <fouadnori@gmail.com>
2022-03-27 23:25:54 +0200

    update assignment1_day3

--- Assignments_w1/Assignment1_day3.cpp
+++ Assignments_w1/Assignment1_day3.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <algorithm>
+#include <math.h>
 using namespace std;
+
 /*
 Write a program that removes the repeated number of an input sorted array and return a new array without
 the repeated numbers. The function shall return error if the size of the input array is ZERO. The function takes
@@ -45,53 +47,47 @@ void PrintOutOld(int arr_old[], int n){
 
 //Function to remove dublicates
 int RemoveDuplicates(int arr_old[], int size_old, int arr_new[], int *size_new){
-    int j=0;
+    //std::cout << *size_new << " ";
+    int temp[size_old];
+    int j = 0;
     int i;
-    int number;
-    //compare one element to the next element and if the element is not equal to the one before
-    //add the same element in the temp array.
-    for( i = 0; i < size_old; i++){
-       int x =0;
-        for (j =  i+1; j < size_old; j++)
-        {
-            number = arr_old[j-1];      
-          //  std::cout << number<< endl;
-            if (number != arr_old[j] ){
-                 arr_new[j-1] = arr_old[j-1];
-                 if (arr_new[j] == 0){
-                     
-                 }
-           }
-        
-        }
-            std::cout << arr_new[i] << " ";   
+    if (size_old == 0 || size_old == 1){
+        return size_old;
     }
-
-    std::cout << std::endl;
-    *size_new = sizeof(arr_new)/sizeof(arr_new[0]);
-    cout << "size of the new array: "<< *size_new << endl;
+  
+    for (i = 0; i < size_old - 1; i++)
+        if (arr_old[i] != arr_old[i + 1])
+            temp[j++] = arr_old[i];
+            temp[j++] = arr_old[size_old - 1];
+    
+    for (i = 0; i < j; i++)
+        arr_new[i] = temp[i];
+    
     return j;
+    
 }
 
 //Print out the new array without dublicates
 void PrintOutNew(int arr_new[], int n){
     for (int i = 0; i<n; i++){
             std::cout << arr_new[i] << " ";
-            std::cout << std::endl;
+           // std::cout << std::endl;
         }
 }
 
 int main(){
+    int n;
     int arr_old[] = {3,5,3,1,2,4,2,5,4,3};
     int size_old = sizeof(arr_old)/sizeof(arr_old[0]);
     cout << "size of the old array: "<< size_old << endl;
     int arr_new[size_old] = {};
- //   int size_new = sizeof(arr_new)/sizeof(arr_new[0]);
- //   cout << "size of the new array: "<< size_new << endl;
     Sort(arr_old, size_old);
     PrintOutOld(arr_old, size_old);
-  //  std::unique(arr_old.begin(), arr_old.end());
-    RemoveDuplicates (arr_old, size_old, arr_new, size_new_ptr);
-    
+   n = RemoveDuplicates (arr_old, size_old, arr_new, size_new_ptr);
+    int size_new = sizeof(arr_new)/sizeof(arr_new[0]);
+    cout << "size of the new array: "<< size_new << endl;
+
+    PrintOutNew(arr_new, size_old);
+
 }
 
